33fa80a285d158177009c9d5a0e40fe55893342a
Philip Ribbens <Philip.Ribbens@phila.gov>
2019-05-02 14:44:49 -0400

    Add Inspections (BL) dashboard

--- li_metabase/views/business_licenses.py
+++ li_metabase/views/business_licenses.py
@@ -7,8 +7,8 @@ from li_metabase.auth import auth
 BUSINESS_LICENSES_DASHBOARDS = [
     Dashboard('Expiring Business Licenses', 'expiring-licenses', 47),
     Dashboard('Business Licenses Incomplete Processes', 'incomplete-processes', 156),
-    Dashboard('Business Licenses Jobs By Submission Mode', 'submission-mode', 155),
-    Dashboard('Business Licenses Overdue Inspections', 'overdue-inspections', 157),
+    Dashboard('Business License Jobs By Submission Mode', 'submission-mode', 155),
+    Dashboard('Business License Inspections', 'inspections', 160),
     Dashboard('Business Licenses Issued', 'issued', 34)
 ]
 
